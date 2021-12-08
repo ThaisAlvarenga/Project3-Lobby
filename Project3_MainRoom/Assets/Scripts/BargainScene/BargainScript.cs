6dd4b9f465828fd456b7c948dccb4cde4cf13344
NYUAD <nyuad@C3-019-IMACP-03.local>
2021-12-08 18:05:44 +0400

    Add Prof's changes

--- Project3_MainRoom/Assets/Scripts/BargainScene/BargainScript.cs
+++ Project3_MainRoom/Assets/Scripts/BargainScene/BargainScript.cs
@@ -10,15 +10,15 @@ public class BargainScript : MonoBehaviour
 
     private void Start()
     {
-        greenGate = GameObject.FindGameObjectWithTag("GreenGate");
-
+        // greenGate = GameObject.Find("gateGreen").transform.GetChild(5);
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Bargain"))
         {
             gameObject.GetComponent<MeshRenderer>().material = right;
-            greenGate.GetComponent<LoadScene>().canUnload = true;
+            GameObject.Find("gateGreen").transform.GetChild(5).GetComponent<LoadScene>().canUnload = true;
         } else
         {
             gameObject.GetComponent<MeshRenderer>().material = wrong;
